687726c35f85fceafc984e208b0e43eb46fe6d95
Ythsc0529 <hsc0529.myself@gmail.com>
2025-12-19 23:51:02 +0800

    netlify

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                app: resolve(__dirname, 'app.html'),
+            },
+        },
+    },
+});
